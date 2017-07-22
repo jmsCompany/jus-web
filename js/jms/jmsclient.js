60684455d9315adf06d9deb70148c0aafed42ddb
junyu <jsliujunyu@163.com>
2017-07-22 17:13:52 +0800

    bug修复

--- js/jms/jmsclient.js
+++ js/jms/jmsclient.js
@@ -4,7 +4,7 @@
      $clientURL = "http://192.168.1.101:9998/jms/"
 
     // $clientURL = "http://118.178.94.7:9998/jms/"
-	//$.fn.dataTable.ext.errMode = 'throw';
+	$.fn.dataTable.ext.errMode = 'throw';
 
 	//$clientURL = "http://localhost:9998/jms/"
 
